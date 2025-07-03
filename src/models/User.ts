f54ab916aebf360d52494cd1a164d0c131b371d6
Angie <angel890817@hotmail.com>
2025-07-03 11:15:41 +1000

    feat:bcrypt to secure the login

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     firstName: {type: String, required:true},
@@ -11,5 +12,24 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
 }, {timestamps: true}
 );
+
+// Presave: run before the user document is saved
+userSchema.pre("save", async function (next) {
+    // skip hashing when the password does not changed
+    if (!this.isModified("password")) return next();
+    // if the password is new or changed
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err as Error);
+    }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword: string) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 export const User = mongoose.model('User', userSchema);
